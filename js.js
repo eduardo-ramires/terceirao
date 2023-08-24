d331245ea0af851f96597c53a6d43ca0cf3c796a
duduramires_ <duduramires2005@gmail.com>
2023-08-24 17:53:09 -0300

    Update js.js

--- js.js
+++ js.js
@@ -1,10 +1,10 @@
 const perguntaEresposta = {
   pergunta: [
-      "Qual piloto detém o recorde de mais títulos de Fórmula 1?",
-      "Qual é a cor tradicional dos carros da Scuderia Ferrari na Fórmula 1?",
-      "Qual é a pista conhecida como 'Casa da Indy 500'?",
-      "Que empresa fabrica os carros de Fórmula 1 com o nome de 'Red Bull Racing'?",
-      "Qual piloto é frequentemente chamado de 'Schumi'?",
+      "Quem é geralmente considerado o inventor da máquina a vapor?",
+      "Em que século as máquinas a vapor começaram a ser desenvolvidas?",
+      "Qual a principal fonte de energia utilizada para movimentar as máquinas a vapor?",
+      "Que tipo de combustível era comumente queimado para gerar vapor nas primeiras máquinas a vapor?",
+      "As máquinas a vapor impulsionaram a inovação no transporte, levando ao desenvolvimento de:",
       "Qual foi a primeira corrida da temporada de Fórmula 1 em 2021?",
       "Que lendário piloto brasileiro é conhecido como 'Magic Senna'?",
       "Que pista sediou a primeira corrida noturna de Fórmula 1?",
@@ -33,11 +33,11 @@ const perguntaEresposta = {
       "Qual é o famoso circuito conhecido como 'A Pista da Morte' devido à sua alta velocidade?"
   ],
   respostaCerta: [
-      "Michael Schumacher",
-      "Vermelho",
-      "Indianapolis Motor Speedway",
-      "Red Bull",
-      "Michael Schumacher",
+      "James Watt",
+      "XVII",
+      "Energia térmica",
+      "Carvão",
+      "Locomotivas e barcos a vapor.",
       "Grande Prêmio do Bahrein",
       "Ayrton Senna",
       "Circuito de Marina Bay (Singapura)",
@@ -66,11 +66,11 @@ const perguntaEresposta = {
       "Autódromo Nazionale Monza"
   ],
   respostaErrada1: [
-      "Ayrton Senna",
-      "Azul",
-      "Daytona International Speedway",
-      "Ferrari",
-      "Nico Rosberg",
+      "Nikola tesla",
+      "XX",
+      "Energia química",
+      "Gasolina",
+      "Aviões e barcos a vapor.",
       "Grande Prêmio da Austrália",
       "Rubens Barrichello",
       "Circuito das Américas",
@@ -99,11 +99,11 @@ const perguntaEresposta = {
       "Nürburgring"
   ],
   respostaErrada2: [
-      "Nelson Piquet",
-      "Amarelo",
-      "Silverstone Circuit",
-      "Mercedes",
-      "Fernando Alonso",
+      "Elon Musk",
+      "XIV",
+      "Energia cinética",
+      "Gás natural",
+      "Locomotivas e carros a vapor.",
       "Grande Prêmio do Japão",
       "Nelson Piquet",
       "Circuito de Interlagos",
@@ -164,25 +164,40 @@ function finalizaJogo(){
   document.querySelector('.jogo').style.display = 'none';
   document.querySelector('.scoreboard').style.display = 'block';
   if (pontos > maior_pontuacao){
+    if (nome_player != null){txtNome.textContent = "Parabéns "+ nome_player + " você bateu o recorde!!!"};
+    txtPontos.textContent = "Sua pontuação foi de: " + pontos;
     maior_pontuacao = pontos;
     nome_maior_pontuador = nome_player;
-  } if (pontos >= 200){
-    txtNome.textContent = "Parabéns "+ nome_player + " você foi muito bem!";
+  } else if (pontos >= 400){
+    if (nome_player != null){txtNome.textContent = "Parabéns "+ nome_player + " você foi muito bem!"};
     txtPontos.textContent = "Sua pontuação foi de: " + pontos;
-  } else if(pontos == 150){
+  } else if(pontos == 300){
     txtNome.textContent = "Parabéns "+ nome_player ;
     txtPontos.textContent = "Sua pontuação foi de: " + pontos;
-  } else{
-    txtNome.textContent = "Obrigado por jogar "+ nome_player ;
+  } else if(pontos == null){
+    if (nome_player != null){txtNome.textContent = "Obrigado por jogar "+ nome_player};
+    txtPontos.textContent = "Tu conseguiu zerar bocó";
+  }else{
+    if (nome_player != null){txtNome.textContent = "Obrigado por jogar "+ nome_player};
     txtPontos.textContent = "Sua pontuação foi de: " + pontos;
   }
 }
 
 function getPerguntasErepostas(){
+  if (pontos >= 100){
+    document.getElementById("pontos").textContent = "Pontos: "+ pontos;
+  }else{
+    document.getElementById("pontos").textContent = "Pontos: 00";
+  }
+
   if (perguntasTotais == 5){
     continuando = true
-  } if (perguntasTotais == 5 && pontos <= 100){
-    finalizaJogo()
+  } if (perguntasTotais == 5 && pontos <= 200){
+    setTimeout(() => {
+      container.classList.remove('errado');
+      container.classList.add('container');
+      finalizaJogo()
+    },500);
   } else{
     document.querySelector('.registration-form').style.display = 'none';
     document.querySelector('.jogo').style.display = 'block';
@@ -209,20 +224,19 @@ function getPerguntasErepostas(){
     pergunta.textContent = primeiraPergunta;
     console.log("certa:", primeiraRespostaCerta)
     if (numeroAleatorioPergunta == 1){
-        botaoResposta1.textContent = primeiraRespostaErrada2;
-        botaoResposta2.textContent = primeiraRespostaErrada1;
-        botaoResposta3.textContent = primeiraRespostaCerta;
+      botaoResposta1.textContent = primeiraRespostaErrada2;
+      botaoResposta2.textContent = primeiraRespostaErrada1;
+      botaoResposta3.textContent = primeiraRespostaCerta;
     } else if (numeroAleatorioPergunta == 2){
-        botaoResposta1.textContent = primeiraRespostaCerta;
-        botaoResposta2.textContent = primeiraRespostaErrada1;
-        botaoResposta3.textContent = primeiraRespostaErrada2;
+      botaoResposta1.textContent = primeiraRespostaCerta;
+      botaoResposta2.textContent = primeiraRespostaErrada1;
+      botaoResposta3.textContent = primeiraRespostaErrada2;
     } else{
-        botaoResposta1.textContent = primeiraRespostaErrada1;
-        botaoResposta2.textContent = primeiraRespostaCerta;
-        botaoResposta3.textContent = primeiraRespostaErrada2;
+      botaoResposta1.textContent = primeiraRespostaErrada1;
+      botaoResposta2.textContent = primeiraRespostaCerta;
+      botaoResposta3.textContent = primeiraRespostaErrada2;
     }
   }
-
 }
 
 function verificaResposta(resposta){
@@ -232,41 +246,42 @@ function verificaResposta(resposta){
     container.classList.add('certo');
     setTimeout(() => {
       container.classList.remove('certo');
-    container.classList.add('container');
-    getPerguntasErepostas()
+      container.classList.add('container');
+      getPerguntasErepostas()
     }, 1000);
-    pontos = pontos + 50  
-    console.log('acertou')
+    pontos = pontos + 100 
   } else if (numeroAleatorioPergunta == 2 && resposta == 1){
     container.classList.remove('container');
     container.classList.add('certo');
     setTimeout(() => {
       container.classList.remove('certo');
-    container.classList.add('container');
-    getPerguntasErepostas()
+      container.classList.add('container');
+      getPerguntasErepostas()
     }, 1000);
-    pontos = pontos + 50 
-      console.log('acertou')
+    pontos = pontos + 100
   } else if (numeroAleatorioPergunta == 3 && resposta == 2){
     container.classList.remove('container');
     container.classList.add('certo');
     setTimeout(() => {
       container.classList.remove('certo');
-    container.classList.add('container');
-    getPerguntasErepostas()
+      container.classList.add('container');
+      getPerguntasErepostas()
     },1000);
-    pontos = pontos + 50 
-      console.log('acertou')
+    pontos = pontos + 100
   } else{
     if (continuando){
-      finalizaJogo()
+      setTimeout(() => {
+        container.classList.remove('errado');
+        container.classList.add('container');
+        finalizaJogo()
+      },500);
     }else{
       container.classList.remove('container');
       container.classList.add('errado');
       setTimeout(() => {
         container.classList.remove('errado');
-      container.classList.add('container');
-      getPerguntasErepostas()
+        container.classList.add('container');
+        getPerguntasErepostas()
       }, 1000);
     }
   }
